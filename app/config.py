a3740d688779c802b40b205ae1990e410d7f1aaf
Kate Fu <kakalium1992@gmail.com>
2025-05-11 17:27:55 +0800

    Adding csrf tokesn in all necessary files. Update signup and login HIdden all generated random key by csrf tokens

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    SECRET_KEY= '960610Moon'
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

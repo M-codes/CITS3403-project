a55ef97bed252a5d9c485c510f973150ffcf9c8a
Kate Fu <kakalium1992@gmail.com>
2025-05-16 00:05:39 +0800

    Updating comment in form.py

--- app/form.py
+++ app/form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
 
+# Define the form for user registration
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
